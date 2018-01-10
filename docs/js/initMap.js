0439507819afdadaa19826333a64b35649eb55ce
Akira <154.4k1r4@gmail.com>
2018-01-11 05:19:56 +0600

    2.33 Размечен и стилизован модуль PAGE-FOOTER, написана js-карта.

---
+++ docs/js/initMap.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/*
+***********************************************************************************
+***********************************************************************************
+***
+***                              Настройки Google Map API
+***
+***********************************************************************************
+***********************************************************************************
+*/
+
+(function() {
+
+  window.initMap = function() {
+    var mstore = {lat: 21.0325397, lng: 105.8133641};
+
+    var map = new google.maps.Map(document.querySelector('.page-footer__map-container'), {
+      zoom: 15,
+      center: mstore,
+      disableDefaultUI: true
+    });
+
+    var marker = new google.maps.Marker({
+      position: mstore,
+      map: map
+    });
+  };
+
+})();
